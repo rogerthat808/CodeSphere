72434f1ef87eb5bdaab43f1ebba20d2cc63fbc0a
Rogelio <roger.cruz808@gmail.com>
2024-01-23 13:53:51 -0800

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import CodeEditor from './components/Navbar/body/CodeEditor';
+import CodeEditor from './components/body/CodeEditor';
 
 const App = () => {
 
